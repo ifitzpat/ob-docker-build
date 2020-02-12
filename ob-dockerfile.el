c7260674ad2d9107a99c30181dc2bf7d7b045190
dr. Ian FitzPatrick <ian@ianfitzpatrick.eu>
2020-02-12 09:56:00 +0100

    initial commit

---
+++ ob-dockerfile.el
@@ -0,0 +1,126 @@
+;;; ob-dockerfile.el --- org-babel functions for dockerfile evaluation
+
+;; Copyright (C) Dr. Ian FitzPatrick
+
+;; Author: Dr. Ian FitzPatrick
+;; Keywords: literate programming, reproducible research
+;; Homepage: https://orgmode.org
+;; Version: 0.01
+
+;;; License:
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING.  If not, write to the
+;; Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+;; Boston, MA 02110-1301, USA.
+
+;;; Commentary:
+;;
+;; call dockerfile apply on org babel block
+;;
+
+;;; Requirements:
+
+
+;;; Code:
+;(require 'ob)
+;(require 'ob-ref)
+;(require 'ob-comint)
+;(require 'ob-eval)
+;(require 's)
+
+;; possibly require modes required for your language
+(define-derived-mode dockerfile-mode dockerfile-mode "dockerfile"
+  "Major mode for building docker containers from org-babel."
+  )
+
+
+;; optionally declare default header arguments for this language
+(defvar org-babel-default-header-args:dockerfile '((:tag . nil)(:push . nil)))
+
+;; This function expands the body of a source code block by doing
+;; things like prepending argument definitions to the body, it should
+;; be called by the `org-babel-execute:dockerfile' function below.
+(defun org-babel-expand-body:dockerfile (body params &optional processed-params)
+  "Expand BODY according to PARAMS, return the expanded body."
+  ;(require 'inf-dockerfile) : TODO check if needed
+  body ; TODO translate params to yaml variables
+)
+
+;; This is the main function which is called to evaluate a code
+;; block.
+;;
+;; This function will evaluate the body of the source code and
+;; return the results as emacs-lisp depending on the value of the
+;; :results header argument
+;; - output means that the output to STDOUT will be captured and
+;;   returned
+;; - value means that the value of the last statement in the
+;;   source code block will be returned
+;;
+;; The most common first step in this function is the expansion of the
+;; PARAMS argument using `org-babel-process-params'.
+;;
+;; Please feel free to not implement options which aren't appropriate
+;; for your language (e.g. not all languages support interactive
+;; "session" evaluation).  Also you are free to define any new header
+;; arguments which you feel may be useful -- all header arguments
+;; specified by the user will be available in the PARAMS variable.
+(defun org-babel-execute:dockerfile (body params)
+  "Execute a block of dockerfile code with org-babel.
+This function is called by `org-babel-execute-src-block'"
+  (let* ((vars (org-babel--get-vars params))
+	 (action (if (assoc :action params) (cdr (assoc :action params)) "apply"))
+	 (context (if (assoc :context params) (concat " --context='" (cdr (assoc :context params)) "' ") nil))
+	 )
+    (message "executing dockerfile source code block")
+    (org-babel-eval-dockerfile (concat "dockerfile " context action " -f" ) body)
+    )
+  ;; when forming a shell command, or a fragment of code in some
+  ;; other language, please preprocess any file names involved with
+  ;; the function `org-babel-process-file-name'. (See the way that
+  ;; function is used in the language files)
+  )
+
+
+(defun org-babel-eval-dockerfile (cmd yaml)
+  "Run CMD on BODY.
+If CMD succeeds then return its results, otherwise display
+STDERR with `org-babel-eval-error-notify'."
+  (let ((err-buff (get-buffer-create " *Org-Babel Error*"))
+	(yaml-file (org-babel-temp-file "ob-dockerfile-yaml-"))
+	(output-file (org-babel-temp-file "ob-dockerfile-out-"))
+	exit-code)
+    (with-temp-file yaml-file (insert yaml))
+    (with-current-buffer err-buff (erase-buffer))
+    (setq exit-code
+	  (shell-command (concat cmd " " yaml-file) output-file err-buff)
+	  )
+      (if (or (not (numberp exit-code)) (> exit-code 0))
+	  (progn
+	    (with-current-buffer err-buff
+	      (org-babel-eval-error-notify exit-code (buffer-string)))
+	    (save-excursion
+	      (when (get-buffer org-babel-error-buffer-name)
+		(with-current-buffer org-babel-error-buffer-name
+		  (unless (derived-mode-p 'compilation-mode)
+		    (compilation-mode))
+		  ;; Compilation-mode enforces read-only, but Babel expects the buffer modifiable.
+		  (setq buffer-read-only nil))))
+	    nil)
+	; return the contents of output file
+	(with-current-buffer output-file (buffer-string)))))
+
+
+(provide 'ob-dockerfile)
+;;; ob-dockerfile.el ends here
